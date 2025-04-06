f8d8a018ebfdf1a589e0a8e631f0e55ffcb8a017
AryanS-88 <ssingharyan70@gmail.com>
2025-04-06 17:43:28 +0530

    Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './Components/**/*.{js,ts,jsx,tsx}',
+    './Context/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
